17ad7a77cc43e146b5e08a752ad6e7a58fd55430
Varun N <varun.n2026@gmail.com>
2025-03-01 11:29:05 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -145,10 +145,6 @@ var config_data = `
     }
   ],
   "endgame": [
-    { "name": "Barge Timer",
-      "code": "ebt",
-      "type": "timer"
-    },
     { "name": "Final Robot Status",
       "code": "efs",
       "type":"radio",
